ff89bb66a2b937258b64b42589f3045687fe9ebf
Daniel Haas <daniel.haas@wareload.net>
2024-04-27 04:30:03 +0200

    add example mapper

--- src/main/java/net/wareload/App.kt
+++
@@ -1,7 +0,0 @@
-package net.wareload
-
-
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
-
---
+++ src/main/java/net/wareload/mapper/oidc/CustomProtocolMapper.java
@@ -0,0 +1,57 @@
+package net.wareload.mapper.oidc;
+
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.mappers.*;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomProtocolMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper,
+        OIDCIDTokenMapper, UserInfoTokenMapper {
+
+    public static final String PROVIDER_ID = "custom-protocol-mapper";
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, CustomProtocolMapper.class);
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Token Mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Custom Token Mapper";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Adds a custom text to the claim";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel,
+                            UserSessionModel userSession, KeycloakSession keycloakSession,
+                            ClientSessionContext clientSessionCtx) {
+        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, "custom");
+    }
+}
